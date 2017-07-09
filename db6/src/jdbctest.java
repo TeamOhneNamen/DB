44ece44e8504340cfce2f27c4e0f25ad2174c3e0
Guter Stiel <opi70@web.de>
2017-07-09 18:52:36 +0200

    op

--- db6/src/jdbctest.java
+++ db6/src/jdbctest.java
@@ -12,16 +12,28 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
-class jdbctest {
+class Jdbctest {
 
 	public static void main(String args[]) throws SQLException, ClassNotFoundException, IOException {
-//		String user = JOptionPane.showInputDialog("User");
-//		String passwort = JOptionPane.showInputDialog("Passwort");
+		String user = JOptionPane.showInputDialog("User");
+		String passwort = JOptionPane.showInputDialog("Passwort");
+		String select = JOptionPane.showInputDialog("Select: ");
 		Class.forName("oracle.jdbc.driver.OracleDriver");
-		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@ora14.informatik.haw-hamburg.de:1521:inf14", "acf200", "Ps4lm_23");
+		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@ora14.informatik.haw-hamburg.de:1521:inf14", user, passwort);
 
 		Statement stmt = con.createStatement();
-		ResultSet rset = stmt.executeQuery("Select * from Student");
+		ResultSet rset;
+		ResultSet rset2;
+		if (!select.equals("")) {
+			rset = stmt.executeQuery("Select * from " + select);
+			rset2 = stmt.executeQuery("SELECT * FROM " + select);
+			
+		}else{
+			rset = stmt.executeQuery("Select TABLE_NAME from USER_TABLES");
+			rset2 = stmt.executeQuery("Select TABLE_NAME from USER_TABLES");
+			
+		}
+		
 		ResultSetMetaData rsetmd = (ResultSetMetaData) rset.getMetaData();
 		System.out.println(rsetmd.getColumnName(1));
 		while (((ResultSet) rset).next()) {
@@ -30,10 +42,9 @@ class jdbctest {
 		}
 		
 	    // It creates and displays the table
-	    JTable table = new JTable(buildTableModel(rset));
+	    JTable table = new JTable(buildTableModel(rset, rset2));
 
 	    // Closes the Connection
-
 	    JOptionPane.showMessageDialog(null, new JScrollPane(table));
 		
 		
@@ -46,10 +57,10 @@ class jdbctest {
 
 	}
 	
-	public static DefaultTableModel buildTableModel(ResultSet rs)
+	public static DefaultTableModel buildTableModel(ResultSet rs, ResultSet rs2)
 	        throws SQLException {
 
-	    ResultSetMetaData metaData = rs.getMetaData();
+		ResultSetMetaData metaData = rs.getMetaData();
 
 	    // names of columns
 	    Vector<String> columnNames = new Vector<String>();
@@ -60,14 +71,17 @@ class jdbctest {
 
 	    // data of the table
 	    Vector<Vector<Object>> data = new Vector<Vector<Object>>();
-	    while (rs.next()) {
+	    
+	    
+	    
+	    while (rs2.next()) {
 	        Vector<Object> vector = new Vector<Object>();
 	        for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
-	            vector.add(rs.getObject(columnIndex));
+	            vector.add(rs2.getObject(columnIndex));
 	        }
 	        data.add(vector);
 	    }
-
+	    
 	    return new DefaultTableModel(data, columnNames);
 
 	}
